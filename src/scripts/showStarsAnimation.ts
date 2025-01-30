292f7f4b206348e11ea9a92120b934d79a803155
Artur Zybin <iamarturchik@gmail.com>
2025-01-30 20:51:36 +0300

    add stars animation

---
+++ src/scripts/showStarsAnimation.ts
@@ -0,0 +1,17 @@
+import { getMilestonesPosition } from './getMilestonesPosition'
+
+export function showStarsAnimation() {
+  const mainElement = document.getElementById('main')!
+  const milestonePosition = getMilestonesPosition().first
+
+  const player = document.createElement('lottie-player')
+
+  player.setAttribute('src', '/lottie/stars.json')
+  player.setAttribute('autoplay', '')
+
+  player.classList.add('stars-player')
+  player.style.height = `${(milestonePosition.y * 1.05) / 2}px`
+  player.style.left = `${milestonePosition.x}px`
+
+  mainElement.append(player)
+}
