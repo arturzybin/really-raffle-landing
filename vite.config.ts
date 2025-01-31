e7e7bfb95d92af716972668ee11f7afc48d99469
Artur Zybin <iamarturchik@gmail.com>
2025-01-31 17:56:36 +0300

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import stylelint from 'vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [stylelint({ build: true, cache: false })],
+  plugins: [stylelint({ build: true, cache: false })], 
+  base: '/really-raffle-landing',
   server: {
     open: true,
     port: 5179,
